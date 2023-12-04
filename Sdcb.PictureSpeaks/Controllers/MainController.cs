5a2db453dba4bbba7cec0d13941410b7900b04d9
sdcb <sdflysha@qq.com>
2023-12-04 15:22:49 +0800

    添加项目文件。

---
+++ Sdcb.PictureSpeaks/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sdcb.PictureSpeaks.Controllers;
+
+public class MainController : Controller
+{
+    [Route("")]
+    public IActionResult Index()
+    {
+        return Ok("Hello World");
+    }
+}
---
+++ Sdcb.PictureSpeaks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sdcb.PictureSpeaks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
